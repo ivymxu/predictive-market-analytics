9405a31bf98c233544fe666a77feeb959528958d
Ivy Xu <140064242+ivymxu@users.noreply.github.com>
2025-05-10 16:17:35 +0000

    generate random sample data

--- data/sample_market_data.py
+++ data/sample_market_data.py
@@ -4,3 +4,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# sample dataset
+np.random.seed(50)
+n_samples = 200
+data = {
+    'feature1': np.random.nprmal(50, 10, n_samples),
+    'feature2': np.random.normal(30, 5, n_samples),
+    'feature3': np.random.normal(100, 20, n_samples),
+    'target': np.random.choice([0, 1], size=n_samples),
+}
+
